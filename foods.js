de0e0ce0fb2a242f9a4f9a0af4e165037cd10e33
Damian <emmydollar98@gmail.com>
2021-01-15 18:20:42 +0100

    updted

--- foods.js
+++ foods.js
@@ -1,10 +1,19 @@
-/*export let header2 = document.querySelector('h2');*/
+
+ 
 let price = document.getElementById('unit_price');
 let total = document.getElementById('total');
 let selectTag = document.querySelector('select');
 let media = window.matchMedia('(min-width: 300px) and (max-width: 800px)');
 let head = document.getElementsByTagName('HEAD')[0];
-  
+let foodName = document.querySelector("#food_title");
+let foodPicture = document.querySelector("#image");
+let itemDetail = [ foodName.innerHTML, foodPicture.src]; 
+localStorage.setItem('rawData',JSON.stringify(itemDetail));
+
+let dataArray =JSON.parse( localStorage.getItem('EditedData'));
+  foodName.innerHTML = dataArray[0];
+  foodPicture.src = dataArray[1];
+
   document.getElementById('addCart').addEventListener('click', ()=>{
   document.getElementById('form').style.display = 'block';
   document.getElementById('main_container').style.opacity = '0.3';
@@ -36,16 +45,16 @@ selectTag.addEventListener('change', ()=>{
 (function loadSubscribeForm(){
    let nevDiv = document.createElement('div');
        nevDiv.id = "subcribe";
-let appendForm = '';
- appendForm += `
-        <form method="post" action="mailTO: emmydollar98@gmail.com">
-             <h2>Subcribe to our mailing List</h2>  
+   let appendForm = '';
+       appendForm += `
+       <form method="post" action="mailTO: emmydollar98@gmail.com">
+             <h2>Subcribe to our mailing List</h2>
             <input type="text" id="fName" name="fName" placeholder="First Name">
             <input type="text" id="lName" name=lName placeholder=" Last Name">
             <input type="email" id="email" name="name" placeholder="Email">
             <button type="submit">Subscribe</button>
-        </form>
- `;
+       </form> `;
+       
  nevDiv.innerHTML = appendForm;
  document.querySelector('body').appendChild(nevDiv);  
 })();
@@ -82,18 +91,4 @@ document.getElementById('changed').addEventListener('click', ()=>{
   }
   
 });
-
-/*document.querySelector('body').addEventListener('click', ()=>{
-  if(media.matches){
-    let link = document.querySelectorAll('.links');
-    link.forEach((a)=>{
-      a.style.display = 'none';
-    });
-    document.getElementById('show').style.display = 'block';
-    document.getElementById('changed').style.display = 'none'; 
-  }
-  
-});*/
-
-/*end of the media screen of the screeen less then 800px*/
 
--- res.js
+++ res.js
@@ -1,3 +1,5 @@
+
+
 /*variable declaratoion*/
 let searchInput = document.querySelector('#search_input');
 let navContainer = document.querySelector('.nav_container');
@@ -7,71 +9,95 @@ let foodImage = document.querySelectorAll('.foodimages');
 let autoDiv = document.querySelector('.autofill');
 let foodContainer = document.querySelector('.food_container');
 let index = 0;
-let allFoods = ['chicken', 'bread', 'pizza', 'rice', 'beans', 'yam']; 
+let allFoods = ['chicken', 'bread', 'pizza', 'rice', 'beans', 'yam'];
 let viewButtom = document.querySelectorAll('.view_button');
 let media = window.matchMedia('(min-width: 300px) and (max-width: 800px)');
+var arr = [];
 
 /*end of the varaible declaration*/
 
-
-/*function for the fetching of the foos from the json file*/ 
 formContainer.addEventListener('submit', loadJson);
-/*end of the fetch function of the form on submit*/
 
- function loadJson(event){
-   event.preventDefault();
-   let userChoice = searchInput.value;
+
+/*function for the fetching of the foods from the json file*/
+
+function loadJson(event) {
+  event.preventDefault();
+  let userChoice = searchInput.value;
   fetch('photoInfo.json')
-  .then(response => response.json())
-  .then((data) =>{
-    let genHTML = '';
+    .then(response => response.json())
+    .then((data) => {
+      let genHTML = '';
 
-   data.filter((choice) =>{
-    if(choice.Food_Type.toLowerCase() == userChoice.toLowerCase() && userChoice != '')
-    return choice;      // this filter fuction helps to select the file that has the food type that mathceswith user selection and returns the new array
+       data.filter((choice) => {
+         if (choice.Food_Type.toLowerCase() == userChoice.toLowerCase() && userChoice != '')
+             return choice;      // this filter fuction helps to select the file that has the food type that mathceswith user selection and returns the new array;
+      }).map((result) => {
+      
+             genHTML +=
+                  `<div class="foods">
+                        <img src="${result.url}" class= "foodimages">
+                        <div class="food_info">
+                            <p class="title">${result.title}</p>
+                          
+                        <a href = "items.html" id="${result.id}" class="view_button">View Recipe</a>
+                        </div>
+                        <p class="item_data">Calory: ${result.calory}</p>
+                        <p class="item_data">Diet_Label: 1</p>
+                        <p class="item_data">Health_Label: ${result.health_label}</p>   
+                    </div>
+                    `;
+        arr.push(result)
 
-  }).map((result) =>{
-
-            genHTML += 
-            `<div class="foods">
-                 <img src="${result.url}" class= "foodimages">
-                 <div class="food_info">
-                     <p class="title">${result.title}</p>
-                    <a href="${result.foodUrl}" target="_blank" class="view_button">View Recipe</a>
-                 </div>
-                 <p class="item_data">Calory: ${result.calory}</p>
-                 <p class="item_data">Diet_Label: 1</p>
-                 <p class="item_data">Health_Label: ${result.health_label}</p>   
-             </div>
-            `;
-
-        
-     });
-     foodContainer.innerHTML = genHTML;
-        
-     }).catch((error)=>{
-      alert('food not found: Reason:'+ error);
-     })
+      });
+      foodContainer.innerHTML = genHTML;
+      // function to handle the updating of the items.html
+      
+      let click = document.querySelectorAll("a");
+            click.forEach((b) => {
+            b.addEventListener("click", () => {
+              arr.forEach((items) => {
+                if (items.id == b.id){
+                  var data = JSON.parse(localStorage.getItem('rawData'));
+                   data[0] = items.title;
+                   data[1] = items.url;
+                  localStorage.setItem('EditedData',JSON.stringify(data));
+                }
+                   
+              });
+          //console.log(arr)
+        });
+      });
+    }).catch((error) => {
+      alert('food not found: Reason:' + error);
+    })
   autoDiv.innerHTML = '';
 
 }
 
+
+
+
+/*end of the fetch function of the form on submit*/
+/*function to add click*/
+
+
 /*function for the autofill action of the form elemeent*/
 
-searchInput.addEventListener('keyup', ()=>{
-	let inputValue = searchInput.value;
+searchInput.addEventListener('keyup', () => {
+  let inputValue = searchInput.value;
   autoDiv.innerHTML = '';
-  let searchResult = allFoods.filter((element)=>{
-  	return element.toLowerCase().startsWith(inputValue.toLowerCase());
+  let searchResult = allFoods.filter((element) => {
+    return element.toLowerCase().startsWith(inputValue.toLowerCase());
   });
 
-    searchResult.forEach((result)=>{
-  	let divElement = document.createElement('div');
-  	divElement.innerHTML = result;
-  	divElement.addEventListener('click', ()=>{search_input.value = divElement.innerHTML; searchInput.focus();});
-  	autoDiv.appendChild(divElement);
-  	if(search_input.value== '')
-  	autoDiv.innerHTML='';
+  searchResult.forEach((result) => {
+    let divElement = document.createElement('div');
+    divElement.innerHTML = result;
+    divElement.addEventListener('click', () => { search_input.value = divElement.innerHTML; searchInput.focus(); });
+    autoDiv.appendChild(divElement);
+    if (search_input.value == '')
+      autoDiv.innerHTML = '';
   });
 });
 
@@ -79,30 +105,30 @@ searchInput.addEventListener('keyup', ()=>{
 
 
 /*code section ofr te media sceen at max-width 800px */
-document.getElementById('show').addEventListener('click', ()=>{
-  
- if(media.matches){
+document.getElementById('show').addEventListener('click', () => {
+
+  if (media.matches) {
     let link = document.querySelectorAll('.links');
-        link.forEach((a)=>{
-        a.style.display = 'block';
+    link.forEach((a) => {
+      a.style.display = 'block';
     });
     document.getElementById('show').style.display = 'none';
-    document.getElementById('changed').style.display = 'block';  
-   }
-  
+    document.getElementById('changed').style.display = 'block';
+  }
+
 });
 
 
-document.getElementById('changed').addEventListener('click', ()=>{
-  if(media.matches){
+document.getElementById('changed').addEventListener('click', () => {
+  if (media.matches) {
     let link = document.querySelectorAll('.links');
-    link.forEach((a)=>{
+    link.forEach((a) => {
       a.style.display = 'none';
     });
     document.getElementById('show').style.display = 'block';
-    document.getElementById('changed').style.display = 'none'; 
+    document.getElementById('changed').style.display = 'none';
   }
-  
+
 });
 
 
@@ -110,14 +136,14 @@ document.getElementById('changed').addEventListener('click', ()=>{
 
 /*section for the background slider*/
 setInterval(slider, 8000);
- 
-function slider(){
+
+function slider() {
   let slid = document.querySelector('#slider');
   slid.style.width = '-200%';
   setInterval(slider2, 8500);
 }
 
-function slider2(){
+function slider2() {
   let slid = document.querySelector('#slider');
   slid.style.width = '100%';
   setInterval(slider, 8400);
